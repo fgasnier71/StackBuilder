44006bfe1e37455de310965fab4567a1f81a0578
fgasnier71 <fgasnier71@gmail.com>
2017-01-03 16:01:09 +0100

    Changed version to 3.0.0.13...

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -2,3 +2,5 @@
 
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
+[assembly: AssemblyVersion("3.0.0.13")]
+[assembly: AssemblyFileVersion("3.0.0.13")]
