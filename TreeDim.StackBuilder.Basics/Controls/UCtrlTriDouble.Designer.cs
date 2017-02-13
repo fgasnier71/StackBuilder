a116c1f4205051054678bb5255e865d3c5ae1913
François Gasnier <fgasnier71@gmail.com>
2017-02-13 13:24:29 +0100

    Implemented UCtrlTriDouble control...

--- TreeDim.StackBuilder.Basics/Controls/UCtrlTriDouble.Designer.cs
+++ TreeDim.StackBuilder.Basics/Controls/UCtrlTriDouble.Designer.cs
@@ -28,18 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.nudValueX = new System.Windows.Forms.NumericUpDown();
+            this.nudValueY = new System.Windows.Forms.NumericUpDown();
+            this.nudValueZ = new System.Windows.Forms.NumericUpDown();
+            this.lbName = new System.Windows.Forms.Label();
+            this.lbUnit = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nudValueX)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudValueY)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudValueZ)).BeginInit();
             this.SuspendLayout();
+            // 
+            // nudValueX
+            // 
+            this.nudValueX.DecimalPlaces = 2;
+            this.nudValueX.Location = new System.Drawing.Point(69, 0);
+            this.nudValueX.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.nudValueX.Name = "nudValueX";
+            this.nudValueX.Size = new System.Drawing.Size(60, 20);
+            this.nudValueX.TabIndex = 0;
+            this.nudValueX.ValueChanged += new System.EventHandler(this.nudValue_ValueChanged);
+            // 
+            // nudValueY
+            // 
+            this.nudValueY.DecimalPlaces = 2;
+            this.nudValueY.Location = new System.Drawing.Point(134, 0);
+            this.nudValueY.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.nudValueY.Name = "nudValueY";
+            this.nudValueY.Size = new System.Drawing.Size(60, 20);
+            this.nudValueY.TabIndex = 1;
+            this.nudValueY.ValueChanged += new System.EventHandler(this.nudValue_ValueChanged);
+            // 
+            // nudValueZ
+            // 
+            this.nudValueZ.DecimalPlaces = 2;
+            this.nudValueZ.Location = new System.Drawing.Point(200, 0);
+            this.nudValueZ.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.nudValueZ.Name = "nudValueZ";
+            this.nudValueZ.Size = new System.Drawing.Size(60, 20);
+            this.nudValueZ.TabIndex = 2;
+            this.nudValueZ.ValueChanged += new System.EventHandler(this.nudValue_ValueChanged);
+            // 
+            // lbName
+            // 
+            this.lbName.AutoSize = true;
+            this.lbName.Location = new System.Drawing.Point(0, 4);
+            this.lbName.Name = "lbName";
+            this.lbName.Size = new System.Drawing.Size(35, 13);
+            this.lbName.TabIndex = 3;
+            this.lbName.Text = "Name";
+            // 
+            // lbUnit
+            // 
+            this.lbUnit.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbUnit.AutoSize = true;
+            this.lbUnit.Location = new System.Drawing.Point(265, 4);
+            this.lbUnit.Name = "lbUnit";
+            this.lbUnit.Size = new System.Drawing.Size(24, 13);
+            this.lbUnit.TabIndex = 4;
+            this.lbUnit.Text = "unit";
             // 
             // UCtrlTriDouble
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lbUnit);
+            this.Controls.Add(this.lbName);
+            this.Controls.Add(this.nudValueZ);
+            this.Controls.Add(this.nudValueY);
+            this.Controls.Add(this.nudValueX);
             this.Name = "UCtrlTriDouble";
             this.Size = new System.Drawing.Size(300, 20);
+            this.SizeChanged += new System.EventHandler(this.ValueControl_SizeChanged);
+            ((System.ComponentModel.ISupportInitialize)(this.nudValueX)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudValueY)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudValueZ)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.NumericUpDown nudValueX;
+        private System.Windows.Forms.NumericUpDown nudValueY;
+        private System.Windows.Forms.NumericUpDown nudValueZ;
+        private System.Windows.Forms.Label lbName;
+        private System.Windows.Forms.Label lbUnit;
     }
 }
--- TreeDim.StackBuilder.Basics/Controls/UCtrlTriDouble.cs
+++ TreeDim.StackBuilder.Basics/Controls/UCtrlTriDouble.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#region Using directives
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
@@ -6,14 +7,85 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+#endregion
 
 namespace treeDiM.StackBuilder.Basics.Controls
 {
     public partial class UCtrlTriDouble : UserControl
     {
+        #region Delegates
+        public delegate void onValueChanged(object sender, EventArgs args);
+        #endregion
+
+        #region Events
+        public event onValueChanged ValueChanged;
+        #endregion
+
+        #region Constructor
         public UCtrlTriDouble()
         {
             InitializeComponent();
         }
+        #endregion
+
+        #region Public properties
+        [Browsable(true),
+        EditorBrowsable(EditorBrowsableState.Always),
+        DesignerSerializationVisibility(DesignerSerializationVisibility.Visible),
+        Category("Appearance")]
+        public override string Text
+        {
+            get { return lbName.Text; }
+            set { lbName.Text = value; }
+        }
+        [Browsable(true)]
+        public double ValueX { get { return (double)nudValueX.Value; } set { nudValueX.Value = (decimal)value; } }
+        [Browsable(true)]
+        public double ValueY { get { return (double)nudValueY.Value; } set { nudValueY.Value = (decimal)value; } }
+        [Browsable(true)]
+        public double ValueZ { get { return (double)nudValueZ.Value; } set { nudValueZ.Value = (decimal)value; } }
+        [Browsable(true)]
+        public decimal Minimum
+        {
+            get { return nudValueX.Minimum; }
+            set { nudValueX.Minimum = value; }
+        }
+        [Browsable(true)]
+        public UnitsManager.UnitType Unit
+        {
+            get { return _unitType; }
+            set
+            {
+                _unitType = value;
+                lbUnit.Text = UnitsManager.UnitString(_unitType);
+                int noDecimals = UnitsManager.NoDecimals(_unitType);
+                nudValueX.DecimalPlaces = noDecimals;
+                nudValueY.DecimalPlaces = noDecimals;
+                nudValueZ.DecimalPlaces = noDecimals;
+            }
+        }
+        #endregion
+
+        #region Event handlers
+        private void nudValue_ValueChanged(object sender, EventArgs e)
+        {
+            if (null != ValueChanged) ValueChanged(this, e);
+        }
+        private void ValueControl_SizeChanged(object sender, EventArgs e)
+        {
+            // set nud location
+            nudValueX.Location = new Point(Width - 3 * UCtrlDualDouble.stNudLength - 4 - UCtrlDualDouble.stLbUnitLength, 0);
+            nudValueY.Location = new Point(Width - 2 * UCtrlDualDouble.stNudLength - 2 - UCtrlDualDouble.stLbUnitLength, 0);
+            nudValueZ.Location = new Point(Width - UCtrlDualDouble.stNudLength - UCtrlDualDouble.stLbUnitLength, 0);
+            // set unit location
+            lbUnit.Location = new Point(Width - UCtrlDualDouble.stLbUnitLength + 1, 4);
+        }
+        #endregion
+
+        #region Data members
+        private UnitsManager.UnitType _unitType;
+        public static int stNudLength = 60;
+        public static int stLbUnitLength = 38;
+        #endregion
     }
 }
