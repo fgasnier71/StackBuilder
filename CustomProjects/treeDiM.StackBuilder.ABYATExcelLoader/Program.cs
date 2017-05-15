9f5965a8d34d454081058beffdf7a80526c10a65
fgasnier71 <fgasnier71@gmail.com>
2017-05-15 10:42:13 +0200

    First version of ABYAT Excel Loader sent for testing...

--- CustomProjects/treeDiM.StackBuilder.ABYATExcelLoader/Program.cs
+++ CustomProjects/treeDiM.StackBuilder.ABYATExcelLoader/Program.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Net;
 
 using treeDiM.StackBuilder.Basics;
 #endregion
@@ -23,6 +25,18 @@ namespace treeDiM.StackBuilder.ABYATExcelLoader
 
             // setting unit system to cm/kg
             UnitsManager.CurrentUnitSystem = UnitsManager.UnitSystem.UNIT_METRIC2;
+
+            WebClient webClient = new WebClient();
+            try
+            {
+                string localFilePath = Path.ChangeExtension(Path.GetTempFileName(), "config");
+                webClient.DownloadFile("http://www.plmpack.com/stackbuilder/ABYATExcelLoader.config", localFilePath);
+            }
+            catch (Exception /*ex*/)
+            {
+                MessageBox.Show("Please contact fgasnier@treedim.com");
+                return;
+            }
 
             Application.Run(new FormMain());
         }
