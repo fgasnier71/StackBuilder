9cb50cd5f600a26ea0eefbea1584851912cf852f
François Gasnier <fgasnier71@gmail.com>
2018-08-20 00:56:28 +0200

    Minor modifications...
    * Removed references to unexisting resource files...
    * Added turkish resources...

--- Sources/CustomProjects/treeDiM.StackBuilder.ABYATExcelLoader/Properties/AssemblyInfo.cs
+++ Sources/CustomProjects/treeDiM.StackBuilder.ABYATExcelLoader/Properties/AssemblyInfo.cs
@@ -20,16 +20,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("97ed98f7-15c9-40e0-b9f7-2833432d5271")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
--- Sources/CustomProjects/treeDiM.StackBuilder.ExcelAddIn/Properties/AssemblyInfo.cs
+++ Sources/CustomProjects/treeDiM.StackBuilder.ExcelAddIn/Properties/AssemblyInfo.cs
@@ -22,17 +22,3 @@ using System.Security;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("2fbfc71c-bf69-4242-95f4-4907158965bf")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-
