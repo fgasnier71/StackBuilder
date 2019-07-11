70301635b70587c193aa6a57244ef98b5899f6b0
François Gasnier <fgasnier71@gmail.com>
2019-07-11 15:54:35 +0200

    Published version 3.0.0.94...

---
+++ Sources/WIX/treeDiM.StackBuilder.DesktopMSM/WixHarvestExe.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\WiX Toolset v3.14\bin\heat.exe" dir "..\..\TreeDim.StackBuilder.Desktop\bin\Release" -dr StackBuilderDesktop -cg StackBuilderDesktopGroup -gg -g1 -sf -srd -sreg -var "var.StackBuilderDesktopSourceDir" -o ".\GroupStackBuilderDesktop.wxs"
