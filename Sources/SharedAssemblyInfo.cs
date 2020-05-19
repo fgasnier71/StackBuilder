a6cb0696c68ea554af53d526e42a94479fd959cb
François Gasnier <fgasnier71@gmail.com>
2020-05-19 16:17:03 +0200

    Corrected bug in italian resources...

--- Sources/SharedAssemblyInfo.cs
+++ Sources/SharedAssemblyInfo.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Reflection.AssemblyVersion("3.0.2.12")]
-[assembly: System.Reflection.AssemblyFileVersion("3.0.2.12")]
+[assembly: System.Reflection.AssemblyVersion("3.0.2.13")]
+[assembly: System.Reflection.AssemblyFileVersion("3.0.2.13")]
 
 
